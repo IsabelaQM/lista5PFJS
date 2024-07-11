d19032fa36194c43359e613f46a0259cbcdf51c1
aluno <aluno@udicentro.local>
2024-07-10 21:36:06 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Box from "./components/Box"
+import Title from "./components/Title"
+
+function App() {
+  return (
+    <>
+      <Title title="Acesso à Informação" infos="Veja dados de transparência e governança" />
+      <Box text="Plano de desenvolvimento institucional" />
+    </>
+  )
+}
+
+export default App
